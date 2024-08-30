228c04f48829abdbc4d36c1139331ae52607630e
Axel Padilla <adpadillar25@gmail.com>
2024-08-29 20:16:58 -0600

    Added padding top in android

--- apps/expo/src/components/navigation-layout.tsx
+++ apps/expo/src/components/navigation-layout.tsx
@@ -1,11 +1,14 @@
-import { SafeAreaView, ScrollView, View } from "react-native";
+import { Platform, SafeAreaView, ScrollView, View } from "react-native";
 
 import BottomNavigation from "./navigation";
 
 const NavigationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <View className="relative h-full w-full bg-background">
-      <SafeAreaView className="flex-1">
+      <SafeAreaView
+        className="flex-1"
+        style={{ paddingTop: Platform.OS === "android" ? 60 : 0 }}
+      >
         <ScrollView>{children}</ScrollView>
       </SafeAreaView>
       <View className="absolute bottom-0 left-0 right-0">
